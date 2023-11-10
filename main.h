94a008d47ed6dc56cff03e424e5ca8580b3e2dd4
laycoon001 <ademolao457@gmail.com>
2023-11-10 02:41:45 -0800

    kin

--- main.h
+++ main.h
@@ -50,7 +50,7 @@ typdef struct liststr
 	struct liststr *next;
 } list_t;
 
-/** 
+/**
  * struct passinfo - contain pseudo-argument to pass into a function
  * allowing uniform prototype for function pointer struct
  * @arg: a string generated from getline containing arguments function
@@ -208,5 +208,28 @@ int _setenv(info_t *, char *, char *, char *);
 char *get_history_file(info_t *info);
 int write_history(info_t *info);
 int build_history_list(info_t *info);
-int build_history_list
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
 
+/* toem_lists.c */
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+/* toem_lists1.c */
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t, list_t *);
+
+/* toem_vars.c */
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_var(info_t *);
+int replace_string(char **, char *);
+
+#endif
