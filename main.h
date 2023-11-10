6a2ab095081cd3b659845563138b2dd53f689c3a
laycoon001 <ademolao457@gmail.com>
2023-11-09 18:26:48 -0800

    main

--- main.h
+++ main.h
@@ -199,3 +199,14 @@ int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
 
+/* toem_getenv.c */
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *, char *);
+
+/* toem_history.c */
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int build_history_list(info_t *info);
+int build_history_list
+
