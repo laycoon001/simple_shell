bfab6e4c0de44397712cefec328aad946d90c43d
Dayo <folushooyinkansola@gmail.com>
2023-11-11 03:59:56 -0800

    main

--- main.c
+++ main.c
@@ -18,16 +18,16 @@ int main(int ac, char **av)
 
 	if (ac == 2)
 	{
-		mv = open(av[1], O_RONLY);
+		mv = open(av[1], O_RDONLY);
 
 		if (mv == -1)
 		{
-			if (errno == EACESS)
+			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
 				_eputs(av[0]);
-				_eputs(": 0 : Can't open  ");
+				_eputs(": 0: Can't open  ");
 				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
