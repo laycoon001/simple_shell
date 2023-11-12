09a7cd96fd8e04e5f4a7b686dc9ff27182fe37c6
Dayo <folushooyinkansola@gmail.com>
2023-11-12 01:12:22 -0800

    main

--- main.c
+++ main.c
@@ -6,39 +6,5 @@
  * @av: argument vector
  * Return: 1 on errno, 0 on success
  */
-int main(int ac, char **av)
+int main()
 {
-	info_t data = { INFO_INIT };
-	int mv = 2;
-
-	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (mv)
-			: "=r" (mv));
-
-	if (ac == 2)
-	{
-		mv = open(av[1], O_RDONLY);
-
-		if (mv == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open  ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		data->readfd = mv;
-	}
-	populate_env_list(data);
-	read_history(data);
-	hsh(data, av);
-	return (EXIT_SUCESS);
-}
